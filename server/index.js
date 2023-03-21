dee8f0fc0d07e04508fe5eb204a56898ce511b00
ayyesu <iamdanielkissiedu@gmail.com>
2023-03-21 13:02:39 +0000

    Created: User Registration API

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config({path: './.env'});
+const userRoute = require('./Routes/userRoute');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/users', userRoute);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the chat app api');
