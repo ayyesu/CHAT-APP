be5ee4d5c076043273f338c65c0b5187b2c644c5
ayyesu <iamdanielkissiedu@gmail.com>
2023-03-30 16:25:27 +0000

    Error: fixed

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/find/:firstId/:secondId', findUser);
+router.get('/find/:userId', findUser);
 router.get('/', getUsers);
 
 module.exports = router;
