df72f0f5a70cc0e30297834a8a79f2cea525b4e6
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-15 18:05:47 +0000

    implemented socketio in chats

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -1,16 +1,22 @@
+import {useContext} from 'react';
 import {useFetchRecipientUser} from '../../hooks/useFetchRecipient';
 import avatar from '../../assets/avatar.svg';
+import {ChatContext} from '../../context/ChatContext';
 
 const UserChat = ({chat, user}) => {
     const {recipientUser} = useFetchRecipientUser(chat, user);
+    const {messages, onlineUsers} = useContext(ChatContext);
 
+    const isOnline = onlineUsers?.some(
+        (user) => user?.userId === recipientUser?._id,
+    );
     return (
         <div className='flex mt-2' role='button'>
             <div className='user-card items-center p-2 justify-between'>
                 <div className='flex'>
                     <div className='mr-4'>
                         <img src={avatar} />
-                        <span className='user-online'></span>
+                        <span className={isOnline ? 'user-online' : ''}></span>
                     </div>
                     <div className='ml-2 text-content'>
                         <div className='text-sm'>{recipientUser?.name}</div>
